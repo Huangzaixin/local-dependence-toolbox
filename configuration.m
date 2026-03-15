0c6f59aa745ac6e0951604a2fef25b2a5b760bcf
Dr. Huang Zaixin <hzx-527@163.com>
2026-03-15 10:49:06 +0800

    Add files via upload

---
+++ configuration.m
@@ -0,0 +1,11 @@
+% Description: Add all necessary directories to MATLAB
+addpath('functions/');  
+addpath('ld_surfaces/');
+addpath('simulation_study/');
+addpath('copula_estimation/');
+addpath('data_examples/');
+addpath('data_examples/example_1/');
+addpath('data_examples/example_2/');
+addpath('quantile_dependence/');
+rehash;
+disp("Load directory complete.");
